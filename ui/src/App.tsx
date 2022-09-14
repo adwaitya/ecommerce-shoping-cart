48dd0a64013f82b1d5becf9d6ec7682dacd24183
adwaitya <adwaitya.118038@gmail.com>
2022-09-14 12:15:51 +0530

    add prodcut list page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,10 @@
 import './index.scss';
 import Home from './pages/Home/Home';
+import ProductList from './pages/Home/ProductList';
 
 const App = () => {
   return (
-    <Home/>
+    <ProductList/>
   )
 }
 
