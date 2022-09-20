53e37e64c0befd62bb1785d8ff1197de5d807670
adwaitya <adwaitya.118038@gmail.com>
2022-09-20 20:50:42 +0530

    add all routes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,15 +1,38 @@
-
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './index.scss';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Login from "./pages/Login";
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 import Register from './pages/Register';
 
 const App = () => {
-  return (
-    <Home/>
-  )
+
+  render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/products/:category" element={<ProductList />}>
+        </Route>
+        <Route path="/product/:id" element={<Product />}>
+        </Route>
+        <Route path="/cart" element={<Cart />}>
+        </Route>
+        <Route path="/login" element={<Login />}>
+        </Route>
+        <Route path="/register" element={<Register />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
 }
 
 export default App
